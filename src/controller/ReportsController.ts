2e7178989a395b3e03378f255a73870647e5c22d
Scott Duncombe <scott.duncombe@zapier.com>
2020-10-22 18:03:41 -0700

    if we have ordered for a url - never trigger a zap

--- src/controller/ReportsController.ts
+++ src/controller/ReportsController.ts
@@ -28,7 +28,7 @@ export class ReportsController {
       extra
     );
 
-    if ((isUnique || willReceive) && !hasTruck) {
+    if ((isUnique || willReceive) && !hasTruck && !alreadyOrdered) {
       if (report.location.validatedAt) {
         await zapNewReport(report);
       } else {
