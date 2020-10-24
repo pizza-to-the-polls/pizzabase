578a9f9be8f1f418f6dd347ba7bd013633f35ca0
Scott Duncombe <scott.duncombe@zapier.com>
2020-10-24 07:57:36 -0700

    No spaces in upload

--- src/entity/Upload.ts
+++ src/entity/Upload.ts
@@ -67,7 +67,9 @@ export class Upload extends BaseEntity {
     upload.ipAddress = ipAddress;
     upload.filePath = `uploads/${city}-${state}-${
       uuidv4().split("-")[0]
-    }.${fileExt}`.toLowerCase();
+    }.${fileExt}`
+      .toLowerCase()
+      .replace(/\s/g, "-");
 
     await upload.save();
 
