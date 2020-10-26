2b7dccb516e9fa9be579889c907b4751c482277e
Scott Duncombe <scott.duncombe@zapier.com>
2020-10-25 19:57:17 -0700

    Migrate action column names to snake case

--- src/entity/Action.ts
+++ src/entity/Action.ts
@@ -15,30 +15,50 @@ export class Action extends BaseEntity {
   @CreateDateColumn({ name: "created_at" })
   createdAt;
 
-  @Column()
+  @Column({ nullable: true })
   @Index()
   user: string;
 
-  @Column()
+  @Column({ nullable: true })
   @Index()
   actionType: string;
 
-  @Column()
+  @Column({ nullable: true })
   @Index()
   entityId: number;
 
-  @Column()
+  @Column({ nullable: true })
   @Index()
   entityType: string;
+
+  @Column({ name: 'user_id', nullable: true })
+  @Index()
+  user_id: string;
+
+  @Column({ name: 'action_type', nullable: true })
+  @Index()
+  action_type: string;
+
+  @Column({ name: 'entity_id', nullable: true })
+  @Index()
+  entity_id: number;
+
+  @Column({ name: 'entity_type', nullable: true })
+  @Index()
+  entity_type: string;
 
   static async log(entity, actionType, user?: string): Promise<Action> {
     const action = new this();
 
     action.user = user || "not specified";
+    action.user_id = user || "not specified";
     action.actionType = actionType;
+    action.action_type = actionType;
 
     action.entityType = entity.constructor.name;
+    action.entity_type = entity.constructor.name;
     action.entityId = entity.id;
+    action.entity_id = entity.id;
 
     await action.save();
 
