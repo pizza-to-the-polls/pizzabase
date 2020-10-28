33902901d2915f97ec255492c9e0a162b9955626
Kyle Ribordy <dev@ribordy.tech>
2020-10-27 22:04:43 -0700

    Fixes donation controller lint offenses

--- src/controller/DonationsController.test.ts
+++ src/controller/DonationsController.test.ts
@@ -1,8 +1,8 @@
 import * as http_mocks from "node-mocks-http";
 import { DonationsController } from "./DonationsController";
-import Stripe from 'stripe';
+import Stripe from "stripe";
 
-jest.mock('stripe');
+jest.mock("stripe");
 
 const controller = new DonationsController();
 
@@ -13,9 +13,9 @@ describe("#create", () => {
     mockStripeClient = {
       checkout: {
         sessions: {
-          create: jest.fn(async () => ({ id: "returned_id" }))
-        }
-      }
+          create: jest.fn(async () => ({ id: "returned_id" })),
+        },
+      },
     };
     (Stripe as any).mockImplementation(() => mockStripeClient);
 
@@ -25,66 +25,76 @@ describe("#create", () => {
   });
 
   it("constructs a Stripe client using our secret key", async () => {
-    process.env.STRIPE_SECRET_KEY = "STRIPE SECRET KEY"
+    process.env.STRIPE_SECRET_KEY = "STRIPE SECRET KEY";
     await controller.create(
       http_mocks.createRequest({ body: {} }),
       http_mocks.createResponse(),
       () => undefined
     );
-    expect(Stripe).toHaveBeenCalledWith("STRIPE SECRET KEY", { apiVersion: "2020-08-27" });
+    expect(Stripe).toHaveBeenCalledWith("STRIPE SECRET KEY", {
+      apiVersion: "2020-08-27",
+    });
   });
 
-  it("creates a Stripe Checkout session", async() => {
+  it("creates a Stripe Checkout session", async () => {
     const body = await controller.create(
-      http_mocks.createRequest({ body: { amountUsd: 10, referrer: "http://google.com" } }),
+      http_mocks.createRequest({
+        body: { amountUsd: 10, referrer: "http://google.com" },
+      }),
       http_mocks.createResponse(),
       () => undefined
     );
     expect(mockStripeClient.checkout.sessions.create).toHaveBeenCalledWith({
-      payment_method_types: ['card'],
-      line_items: [{
-        description: 'About 1 pizza',
-        price_data: {
-          product: "stripe_product_12345",
-          unit_amount: 1000,
-          currency: 'usd',
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          description: "About 1 pizza",
+          price_data: {
+            product: "stripe_product_12345",
+            unit_amount: 1000,
+            currency: "usd",
+          },
+          quantity: 1,
         },
-        quantity: 1,
-      }],
-      mode: 'payment',
-      success_url: 'http://polls.pizza/donate?success=true&amount_usd=10',
-      cancel_url: 'http://polls.pizza/donate',
+      ],
+      mode: "payment",
+      success_url: "http://polls.pizza/donate?success=true&amount_usd=10",
+      cancel_url: "http://polls.pizza/donate",
       metadata: {
-        referrer: "http://google.com"
-      }
-    })
+        referrer: "http://google.com",
+      },
+    });
 
     expect(body.checkoutSessionId).toEqual("returned_id");
   });
 
   it("pluralizes pizzas", async () => {
     await controller.create(
-      http_mocks.createRequest({ body: { amountUsd: 100, referrer: "http://google.com" } }),
+      http_mocks.createRequest({
+        body: { amountUsd: 100, referrer: "http://google.com" },
+      }),
       http_mocks.createResponse(),
       () => undefined
     );
     expect(mockStripeClient.checkout.sessions.create).toHaveBeenCalledWith({
-      payment_method_types: ['card'],
-      line_items: [{
-        description: 'About 5 pizzas',
-        price_data: {
-          product: "stripe_product_12345",
-          unit_amount: 10000,
-          currency: 'usd',
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          description: "About 5 pizzas",
+          price_data: {
+            product: "stripe_product_12345",
+            unit_amount: 10000,
+            currency: "usd",
+          },
+          quantity: 1,
         },
-        quantity: 1,
-      }],
-      mode: 'payment',
-      success_url: 'http://polls.pizza/donate?success=true&amount_usd=100',
-      cancel_url: 'http://polls.pizza/donate',
+      ],
+      mode: "payment",
+      success_url: "http://polls.pizza/donate?success=true&amount_usd=100",
+      cancel_url: "http://polls.pizza/donate",
       metadata: {
-        referrer: "http://google.com"
-      }
-    })
+        referrer: "http://google.com",
+      },
+    });
   });
 });
--- src/controller/DonationsController.ts
+++ src/controller/DonationsController.ts
@@ -9,9 +9,9 @@ export class DonationsController {
 
     try {
       const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-        apiVersion: '2020-08-27',
+        apiVersion: "2020-08-27",
       });
-      
+
       event = stripe.webhooks.constructEvent(
         JSON.stringify(request.body),
         request.headers["stripe-signature"],
@@ -35,10 +35,7 @@ export class DonationsController {
         await Donation.failCharge("refunded", event.data.object);
         break;
       case "charge.dispute.funds_withdrawn":
-        await Donation.failCharge(
-          "dispute.funds_withdrawn",
-          event.data.object
-        );
+        await Donation.failCharge("dispute.funds_withdrawn", event.data.object);
         break;
       default:
         console.log(`Unhandled event type ${event.type}`);
@@ -48,40 +45,41 @@ export class DonationsController {
   }
 
   async create(request: Request, _response: Response, _next: NextFunction) {
-    const {
-      amountUsd,
-      referrer
-    } = request.body;
+    const { amountUsd, referrer } = request.body;
 
     const numberOfPizzas = Math.ceil(amountUsd / 20);
     try {
       const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-        apiVersion: '2020-08-27',
-      });    
+        apiVersion: "2020-08-27",
+      });
 
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [{
-          description: `About ${numberOfPizzas} ${numberOfPizzas === 1 ? 'pizza' : 'pizzas'}`,
-          price_data: {
-            product: process.env.DONATION_PRODUCT_ID,
-            unit_amount: amountUsd * 100,
-            currency: 'usd',
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            description: `About ${numberOfPizzas} ${
+              numberOfPizzas === 1 ? "pizza" : "pizzas"
+            }`,
+            price_data: {
+              product: process.env.DONATION_PRODUCT_ID,
+              unit_amount: amountUsd * 100,
+              currency: "usd",
+            },
+            quantity: 1,
           },
-          quantity: 1,
-        }],
-        mode: 'payment',
+        ],
+        mode: "payment",
         metadata: {
-          referrer
+          referrer,
         },
         success_url: `${process.env.CLIENT_APP_URL}/donate?success=true&amount_usd=${amountUsd}`,
         cancel_url: `${process.env.CLIENT_APP_URL}/donate`,
-      });        
-        
+      });
+
       return { success: true, checkoutSessionId: session.id };
     } catch (e) {
       console.error(e);
-      return { success: false, message: e.message }
+      return { success: false, message: e.message };
     }
   }
 }
