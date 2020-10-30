ef5bebd40111273e11c4f6f3c06101939d1ed829
Pramod Thammaiah <pramod.thammaiah+git@gmail.com>
2020-10-30 16:17:42 -0700

    Run fix

--- scripts/populateHistoricalDonations.ts
+++ scripts/populateHistoricalDonations.ts
@@ -2,7 +2,7 @@ import { Donation } from "../src/entity/Donation";
 
 // See Stripe documentation here: https://stripe.com/docs/api/charges/list?lang=node
 
-const stripe = require('stripe')('sk_test_23aRsCkc4f9KwNcYm5fKUtQY');
+const stripe = require("stripe")("sk_test_23aRsCkc4f9KwNcYm5fKUtQY");
 const maxChargesPerPage = 100; // 1 to 100
 
 (async () => {
@@ -14,10 +14,10 @@ const maxChargesPerPage = 100; // 1 to 100
 
   // Paginate until we run out of transactions.
   while (charges.data.length > 0) {
-    for (let index in charges.data) {
-      let charge = charges.data[index];
+    for (const index in charges.data) {
+      const charge = charges.data[index];
       console.log(charge.id);
-      // save the charge if not refunded 
+      // save the charge if not refunded
       if (charge.amount > 0 && charge.amount_refunded == 0) {
         let donation = await Donation.succeedCharge(charge);
         donation.createdAt = charge.created;
@@ -25,11 +25,11 @@ const maxChargesPerPage = 100; // 1 to 100
         donation.save();
       }
     }
-    // Get the next page. 
+    // Get the next page.
     console.log("Getting Next page");
     charges = await stripe.charges.list({
       limit: maxChargesPerPage,
-      starting_after: charges.data[charges.data.length - 1].id
+      starting_after: charges.data[charges.data.length - 1].id,
     });
   }
 })();
