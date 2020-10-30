87525eb526c2966c88a2adda5f6fce2e70466b93
Kyle Ribordy <dev@ribordy.tech>
2020-10-29 20:57:04 -0700

    Adds Donations "create" Action for Stripe Checkout (#24)
    This handles creating a Stripe Checkout session for a given donation
    amount

--- src/controller/DonationsController.ts
+++ src/controller/DonationsController.ts
@@ -1,15 +1,17 @@
 import { NextFunction, Request, Response } from "express";
-import * as Stripe from "stripe";
+import Stripe from "stripe";
 
 import { Donation } from "../entity/Donation";
-
-const stripe = Stripe(process.env.STRIPE_KEY);
 
 export class DonationsController {
   async webhook(request: Request, response: Response, _next: NextFunction) {
     let event;
 
     try {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+        apiVersion: "2020-08-27",
+      });
+
       event = stripe.webhooks.constructEvent(
         JSON.stringify(request.body),
         request.headers["stripe-signature"],
@@ -20,28 +22,62 @@ export class DonationsController {
       return `Webhook Error: ${err.message}`;
     }
 
-    const { type, data } = request.body;
-
     switch (event.type) {
       case "charge.succeeded":
-        await Donateion.succeedCharge(event.data.object);
+        await Donation.succeedCharge(event.data.object);
         break;
       case "charge.failed":
-        await Donateion.failCharge("failed", event.data.object);
+        await Donation.failCharge("failed", event.data.object);
         break;
       case "charge.refunded":
-        await Donateion.failCharge("refunded", event.data.object);
+        await Donation.failCharge("refunded", event.data.object);
         break;
       case "charge.dispute.funds_withdrawn":
-        await Donateion.failCharge(
-          "dispute.funds_withdrawn",
-          event.data.object
-        );
+        await Donation.failCharge("dispute.funds_withdrawn", event.data.object);
         break;
       default:
         console.log(`Unhandled event type ${event.type}`);
     }
 
     return { success: true };
+  }
+
+  async create(request: Request, _response: Response, _next: NextFunction) {
+    const { amountUsd, referrer } = request.body;
+
+    const numberOfPizzas = Math.ceil(amountUsd / 20);
+    try {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+        apiVersion: "2020-08-27",
+      });
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            description: `About ${numberOfPizzas} ${
+              numberOfPizzas === 1 ? "pizza" : "pizzas"
+            }`,
+            price_data: {
+              product: process.env.STRIPE_PRODUCT_ID,
+              unit_amount: amountUsd * 100,
+              currency: "usd",
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        metadata: {
+          referrer,
+        },
+        success_url: `${process.env.STATIC_SITE}/donate?success=true&amount_usd=${amountUsd}`,
+        cancel_url: `${process.env.STATIC_SITE}/donate`,
+      });
+
+      return { success: true, checkoutSessionId: session.id };
+    } catch (e) {
+      console.error(e);
+      return { success: false, message: e.message };
+    }
   }
 }
