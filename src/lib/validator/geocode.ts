f107b0e53c85c4fcc29c4da0bdc66e73da457310
Scott Duncombe <scott.duncombe@shopify.com>
2024-10-30 15:32:20 -0700

    Fix geocoding issue for place

--- src/lib/validator/geocode.ts
+++ src/lib/validator/geocode.ts
@@ -9,6 +9,7 @@ const GMAPS_COMPONENT_MAPPING = {
   route: "street",
   street_number: "num",
   administrative_area_level_1: "state",
+  premise: "premise",
 };
 
 import { NormalAddress } from "./types";
@@ -31,22 +32,27 @@ const gmapsGeocode = async (body: string): Promise<null | NormalAddress> => {
       address_components,
     } = result;
 
-    const { city, state, zip, num, street } = address_components.reduce(
-      (obj, { short_name, types }) => {
-        for (const type of types) {
-          if (GMAPS_COMPONENT_MAPPING[type]) {
-            obj[GMAPS_COMPONENT_MAPPING[type]] = short_name;
-          }
+    const {
+      city,
+      state,
+      zip,
+      num,
+      street,
+      premise,
+    } = address_components.reduce((obj, { short_name, types }) => {
+      for (const type of types) {
+        if (GMAPS_COMPONENT_MAPPING[type]) {
+          obj[GMAPS_COMPONENT_MAPPING[type]] = short_name;
         }
-        return obj;
-      },
-      {}
-    );
-    if (!num || !street || !city || !state || !zip) {
+      }
+      return obj;
+    }, {});
+    const address = num && street ? `${num} ${street}` : premise;
+
+    if (!address || !city || !state || !zip) {
       return null;
     }
 
-    const address = `${num} ${street}`;
     const fullAddress = `${address} ${city} ${state} ${zip}`;
 
     return {
