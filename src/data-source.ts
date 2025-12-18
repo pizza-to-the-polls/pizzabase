7e64f65f9daa943dd8a83911428879690f21aa02
Scott Duncombe <srduncombe@gmail.com>
2025-12-17 23:47:42 -0800

    Add migration

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
-
+import { DataSource, DataSourceOptions } from "typeorm";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { AuroraPostgresConnectionOptions } from "typeorm/driver/aurora-postgres/AuroraPostgresConnectionOptions";
 import { Action } from "./entity/Action";
 import { Donation } from "./entity/Donation";
 import { Location } from "./entity/Location";
@@ -9,37 +10,67 @@ import { Report } from "./entity/Report";
 import { Truck } from "./entity/Truck";
 import { Upload } from "./entity/Upload";
 
+const entities = [Action, Donation, Location, Order, Report, Truck, Upload];
+const migrations = ["src/migration/**/*.ts"];
+const subscribers = ["src/subscriber/**/*.ts"];
 
-export let AppDataSource: DataSource;
+const prodConfig: AuroraPostgresConnectionOptions = {
+  type: "aurora-postgres",
+  database: process.env.DB_NAME,
+  resourceArn: process.env.AURORA_RESOURCE_ARN,
+  secretArn: process.env.AURORA_SECRET_ARN,
+  region: process.env.AWS_REGION,
+  formatOptions: {
+    castParameters: true, // Recommended for Aurora Data API
+  },
+  synchronize: false,
+  migrationsRun: true,
+  entities,
+  migrations,
+  subscribers,
+};
 
-export const initializeDataSource = (source?: DataSource): DataSource => {
-  if (source) {
-    AppDataSource = source;
+const devConfig: PostgresConnectionOptions = {
+  type: "postgres",
+  port: parseInt(process.env.POSTGRES_PORT || "5432"),
+  username: process.env.POSTGRES_USERNAME || "postgres",
+  database: process.env.POSTGRES_DB || "pizzabase",
+  password: process.env.POSTGRES_PASSWORD,
+  synchronize: false,
+  migrationsRun: false,
+  entities,
+  migrations,
+  subscribers,
+};
+
+const testConfig: PostgresConnectionOptions = {
+  type: "postgres",
+  port: 5432,
+  username: process.env.POSTGRES_USERNAME || "postgres",
+  database: `${process.env.POSTGRES_DB || "pizzabase"}Test`,
+  password: process.env.POSTGRES_PASSWORD,
+  dropSchema: true,
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+};
+
+let options: DataSourceOptions;
+if (process.env.NODE_ENV === "prod") {
+  options = prodConfig;
+} else if (process.env.NODE_ENV === "test") {
+  options = testConfig;
+} else {
+  options = devConfig;
+}
+
+export const AppDataSource = new DataSource(options);
+
+export const initializeDataSource = async () => {
+  if (AppDataSource.isInitialized) {
+    return AppDataSource;
   }
-
-  if (AppDataSource) return AppDataSource;
-
-  AppDataSource = new DataSource({
-    type: "postgres",
-    port: parseInt(process.env.POSTGRES_PORT || "5432"),
-    username: process.env.POSTGRES_USERNAME || "postgres",
-    database: process.env.POSTGRES_DB || "pizzabase",
-    password: process.env.POSTGRES_PASSWORD,
-
-    synchronize: false,
-    logging: false,
-    entities: [
-      Action,
-      Donation,
-      Location,
-      Order,
-      Report,
-      Truck,
-      Upload,
-    ],
-    migrationsRun: process.env.NODE_ENV === "prod",
-    migrations: ["src/migration/**/*.ts"],
-  });
-
-  return AppDataSource;
+  return AppDataSource.initialize();
 };
--- src/tests/dbHelper.ts
+++ src/tests/dbHelper.ts
@@ -1,28 +1,8 @@
-import { DataSource, DataSourceOptions } from "typeorm";
-import { initializeDataSource } from "../data-source";
-
-let dataSource: DataSource;
+import { AppDataSource, initializeDataSource } from "../data-source";
 
 const setUpDB = async () => {
-  const config: DataSourceOptions = {
-    type: "postgres",
-    port: 5432,
-    username: process.env.POSTGRES_USERNAME || "postgres",
-    database: process.env.POSTGRES_DB || "pizzabaseTest",
-    password: process.env.POSTGRES_PASSWORD,
-    dropSchema: true,
-    synchronize: true,
-    logging: false,
-    entities: ["src/entity/**/*.ts"],
-    migrations: ["src/migration/**/*.ts"],
-    subscribers: ["src/subscriber/**/*.ts"],
-  };
-
   try {
-    dataSource = new DataSource(config);
-    await dataSource.initialize();
-    // Set this as the global AppDataSource for the app to use
-    initializeDataSource(dataSource);
+    await initializeDataSource();
   } catch (e) {
     console.error("Could not establish db connection - is your schema ok?");
     throw e;
@@ -30,15 +10,15 @@ const setUpDB = async () => {
 };
 
 const closeDB = async () => {
-  if (dataSource && dataSource.isInitialized) {
-    await dataSource.destroy();
+  if (AppDataSource && AppDataSource.isInitialized) {
+    await AppDataSource.destroy();
   }
 };
 
 const getEntities = async () => {
   const exclude = ["api_keys"];
   const entities = [];
-  dataSource.entityMetadatas.forEach(({ name, tableName }) => {
+  AppDataSource.entityMetadatas.forEach(({ name, tableName }) => {
     if (!exclude.includes(tableName)) entities.push({ name, tableName });
   });
   return entities;
@@ -48,7 +28,7 @@ const cleanAll = async () => {
   const entities = await getEntities();
   try {
     for (const entity of entities) {
-      const repository = dataSource.getRepository(entity.name);
+      const repository = AppDataSource.getRepository(entity.name);
       await repository.query(`TRUNCATE TABLE ${entity.tableName} CASCADE;`);
     }
   } catch (error) {
